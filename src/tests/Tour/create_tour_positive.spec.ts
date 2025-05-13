9f8edb55ffae03f7cf3c18a15eecc7c821f4f2bd
Karina Groshnikova <kgroshnikova@gmail.com>
2025-05-12 21:12:15 -0400

    test2

--- src/tests/Tour/create_tour_positive.spec.ts
+++ src/tests/Tour/create_tour_positive.spec.ts
@@ -16,23 +16,7 @@ describe("TOUR CREATE", () => {
     await request
       .post("/tours")
       .set("Cookie", cookie)
-      .send({
-        name: "Test tour2",
-        duration: 10,
-        description: "Could be",
-        maxGroupSize: 10,
-        summary: "Test tour",
-        difficulty: "easy",
-        price: 100,
-        rating: 4.8,
-        imageCover: "tour-3-cover.jpg",
-        ratingsAverage: 4.9,
-        guides: [],
-        startDates: ["2024-04-04"],
-        startLocation: {
-          type: "Point",
-          coordinates: [-74.005974, 40.712776], // [longitude, latitude]
-        },
+      .send(
       }).then(tourRes => {
         console.log(tourRes.body, "tourRes");
         expect(tourRes.statusCode).toBe(201);
